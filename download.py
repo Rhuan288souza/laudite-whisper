1318b091f42617cf0c17b3627261f1b7e3b781e0
Rhuan <rhuan.souza@ewally.com.br>
2023-01-25 14:06:30 -0300

    adjust download

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import AutoModelForCTC, AutoProcessor, Wav2Vec2Processor
 MODEL_NAME = 'laudite-ai/wav2vec2-large-a3500-e30'
 API_KEY = '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'
 
-def download_model():
+def download_model(hotwords=[], use_lm_if_possible = True):
     global model,processor,device
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -25,6 +25,6 @@ def download_model():
     use_lm_if_possible = use_lm_if_possible
 
     model.to(device)
-    
+
 if __name__ == "__main__":
     download_model()
